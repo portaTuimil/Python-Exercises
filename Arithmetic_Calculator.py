768a23c2a0a4622960f4784fe91122d411cd7bd0
Pablo Porta <100687198+DarkProgrammerEs@users.noreply.github.com>
2023-08-24 08:59:07 +0200

    Arithmetic_Calculator fixed, rjust implemented

--- Arithmetic_Calculator.py
+++ Arithmetic_Calculator.py
@@ -1,63 +1,43 @@
-def arithmetic_arranger(listOfNumber, boolOfAnswer):
-    listlen = len(listOfNumber)
+import re
 
-    for i in range(0, listlen):
-        listOfIteration = listOfNumber[i].split()
+def arithmetic_arranger(listOfOperations, answerRequired):
+    for i in range(0, len(listOfOperations)):
+        operation = listOfOperations[i]
+        result = 0
 
-        n0 = len(listOfIteration[0])
-        if n0 >= 4:
-            n0 = 3
-        elif n0 == 3:
-            n0 = 4
-        elif n0 == 2:
-            n0 = 5
-        elif n0 == 1:
-            n0 = 6
+        if '+' in operation:
+            splittedOperation = operation.split('+')
+            result = int(splittedOperation[0]) + int(splittedOperation[1])
+            operator = '+'
+        elif '-' in operation:
+            splittedOperation = operation.split('-')
+            result = int(splittedOperation[0]) - int(splittedOperation[1])
+            operator = '-'
+        elif '/' in operation:
+            splittedOperation = operation.split('/')
+            result = int(splittedOperation[0]) / int(splittedOperation[1])
+            operator = '/'
+        elif '*' in operation:
+            splittedOperation = operation.split('*')
+            result = int(splittedOperation[0]) * int(splittedOperation[1])
+            operator = '*'
 
-        n2 = len(listOfIteration[2])
-        if n2 >= 4:
-            n2 = 0
-        elif n2 == 3:
-            n2 = 1
-        elif n2 == 2:
-            n2 = 2
-        elif n2 == 1:
-            n2 = 3
+        splittedOperation[0].replace(" ", "")
+        splittedOperation[1].replace(" ", "")
 
-        
-        if boolOfAnswer == 'True':
-            if listOfIteration[1] == '+':
-                result = int(listOfIteration[0]) + int(listOfIteration[2])
-            if listOfIteration[1] == '-':
-                result = int(listOfIteration[0]) - int(listOfIteration[2])
+        if answerRequired == True:
+            print('\n', splittedOperation[0].rjust(20) , '\n', 
+                operator.rjust(15), splittedOperation[1].rjust(4), '\n',
+                '------'.rjust(20), '\n',
+                str(result).rjust(20), '\n')
         else:
-            break
+            print('\n', splittedOperation[0].rjust(20) , '\n', 
+                operator.rjust(15), splittedOperation[1].rjust(4), '\n',
+                '------'.rjust(20), '\n')
         
-        lenresult = len(str(result))
 
-        if lenresult == 8:
-            nr = 0
-        elif lenresult == 7:
-            nr = 1
-        elif lenresult == 6:
-            nr = 2
-        elif lenresult == 5:
-            nr = 3
-        elif lenresult == 4:
-            nr = 4
-        elif lenresult == 3:
-            nr = 5
-        elif lenresult == 2:
-            nr = 6
-        else:
-            nr = 7
 
-        print(' '*n0, listOfIteration[0], '\n', listOfIteration[1], ' '*n2,  listOfIteration[2], '\n', '--------')
-
-        print(' '*nr, result, '\n')
-       
 lst = []
-
 while True:
     p0 = input('Introduce an Operation(write <none> if you have finished): ')
     if p0 != 'none':
